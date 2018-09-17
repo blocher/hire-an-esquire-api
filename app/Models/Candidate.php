4b89e1c4e9fe87a6113a64f729b7dcc5330d7422
Benjamin Locher <ben@engagedc.com>
2018-09-16 22:40:57 -0400

    automatic review changing

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -9,4 +9,18 @@ class Candidate extends Model
     
 	public $guarded = ['id', 'created_at', 'updated_at'];
 
+	public static function boot()
+    {
+        parent::boot();
+
+        self::saving(function($model){
+            $original = $model->getOriginal();
+		    if ($original['status'] == 'pending') && $model->stats!='pending' ) {
+		        $model->reviewed = true;
+		    }
+        });
+
+    }
+
+
 }
